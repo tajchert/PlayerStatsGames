99d100f7af069b9cb436e209d83a776b73c98063
Michal Tajchert <mtajchert@gmail.com>
2015-05-09 17:25:48 +0200

    Search view added.

--- app/src/main/java/pl/tajchert/playerstats/ApiConstants.java
+++ app/src/main/java/pl/tajchert/playerstats/ApiConstants.java
@@ -6,7 +6,5 @@ package pl.tajchert.playerstats;
 public class ApiConstants {
     public static String API_URL_IMPORTIO = "https://api.import.io/store/data";
     public static String IMPORTIO_API_USER_KEY = "96da392b-9105-49e7-a2e4-d02f5d883baf";
-    public static String IMPORTIO_API_KEY = "";
-
-    public static final String IMPORTIO_API_WAR_THUNDER_USER = "https://api.import.io/store/data/81005358-b529-4dc9-bfe8-c960593d0952/_query?input/webpage/url=http%3A%2F%2Fwarthunder.com%2Fen%2Fcommunity%2Fuserinfo%2F%3Fnick%3DOddy&_user=96da392b-9105-49e7-a2e4-d02f5d883baf&_apikey=bV4P1GonEEIQ9+sPJQ9JyE9c+ilcroEHyDVM/ZoCCgTxH2zAmma9m4hW1BOjk0Nt+2HqdR1kpbXqwnC4KVRsJQ==";
+    
 }
--- app/src/main/java/pl/tajchert/playerstats/MainActivity.java
+++ app/src/main/java/pl/tajchert/playerstats/MainActivity.java
@@ -1,12 +1,26 @@
 package pl.tajchert.playerstats;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import retrofit.Callback;
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
@@ -16,10 +30,75 @@ import retrofit.client.Response;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @InjectView(R.id.secondEdit)
+    LinearLayout linearSecondLayout;
+
+    @InjectView(R.id.firstEdit)
+    LinearLayout linearFirstLayout;
+
+    @InjectView(R.id.dummyView)
+    View dummyView;
+
+    @InjectView(R.id.gameSelection)
+    Spinner spinnerGameSelection;
+
+    @InjectView(R.id.inputName)
+    EditText userNameEdit;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        setSupportActionBar(toolbar);
+        setSpinnerGameSelection();
+    }
+
+    private void setSpinnerGameSelection(){
+        ArrayList<String> games = new ArrayList();
+        games.add("War Thunder");
+        games.add("World Of Tanks");
+        games.add("Star Craft 2");
+        ArrayAdapter<String> gamesAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, games);
+        gamesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGameSelection.setAdapter(gamesAdapter);
+
+        spinnerGameSelection.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d(TAG, "onItemSelected :" + i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    @OnClick(R.id.firstEdit)
+    public void searchClick() {
+        linearSecondLayout.setVisibility(View.VISIBLE);
+        dummyView.setVisibility(View.VISIBLE);
+    }
+
+    @OnClick(R.id.inputName)
+    public void editTextClick() {
+        linearSecondLayout.setVisibility(View.VISIBLE);
+        dummyView.setVisibility(View.VISIBLE);
+    }
+
+
+    @OnClick(R.id.dummyView)
+    public void dummyClick() {
+        linearSecondLayout.setVisibility(View.GONE);
+        dummyView.setVisibility(View.GONE);
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(userNameEdit.getWindowToken(), 0);
     }
 
     @Override
